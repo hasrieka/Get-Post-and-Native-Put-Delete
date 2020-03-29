658e6f356fb1f8ba5ab08fbf5e468051713f6880
hasrieka <58162829+hasrieka@users.noreply.github.com>
2020-03-29 23:54:38 +0800

    Update put.php

--- put.php
+++ put.php
@@ -28,31 +28,6 @@ $context  = stream_context_create($opts);
 
 $output = file_get_contents('http://localhost/eka_json/update_put.php', false, $context);
 
-
-// $postdatas =
-//     array(
-//     	'nim' => $_POST['nim'],
-//         'nama_mhs' => $_POST['nama_mhs'],
-//         'alamat' => $_POST['alamat']
-// );
-//     $payload = json_encode($postdatas);
-// $ch = curl_init(); 
-
-//     // set url 
-// 					curl_setopt($ch, CURLOPT_URL, 'http://10.0.12.89/mahasiswa/update_putmahasiswa.php');
-// 					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-// 					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-// 					curl_setopt($ch, CURLOPT_POSTFIELDS,$postdata);
-// 					curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-
-//     // return the transfer as a string 
-
-//     // $output contains the output string 
-// 					$output = curl_exec($ch); 
-
-//     // tutup curl 
-// 					curl_close($ch);    
-
 					print_r(json_decode($output,true)); 
 } else {
 	echo "Gagal PUT Data";
